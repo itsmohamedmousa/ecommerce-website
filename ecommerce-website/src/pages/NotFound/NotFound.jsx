a7e1f443a26017473d435a31903239027c4f6ebf
Mohamed Mousa <mohamed312mousa@gmail.com>
2025-02-24 18:24:08 +0200

    Adding a back to home button in the NotFound page

--- ecommerce-website/src/pages/NotFound/NotFound.jsx
+++ ecommerce-website/src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import './notFound.css';
 
 function NotFound() {
+  const navigate = useNavigate();
+  const handleBackToHome = () => navigate('/');
 
   return (
     <div className="not-found">
       <h1>Error 404</h1>
       <h1>Page Not Found</h1>
+      <button onClick={handleBackToHome}>Go Back to Home</button>
     </div>
   );
 }
